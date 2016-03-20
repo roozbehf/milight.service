039bf703d7b2dfa8d6a25ecd19d52e6cb9dfe8ff
Roozbeh Farahbod <research@roozbeh.ca>
2016-03-20 23:57:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Milight Controller Service
+
+This is a simple REST service (not really REST compliant) to control milight RGB lights. The service uses
+[node-milight](https://github.com/oeuillot/node-milight) to communicate to a milight
+controller accessible on the network.
+
+**NOTE** that there is no acknowledgement nor any endorsement from milight.
+
+## Setup and Run
+1. Install node packages:
+```
+npm install
+```
+2. Configure the service by editing `config.json`.
+3. Run the service either by calling node:
+```
+node milight_server.js
+```
+or through npm:
+```
+npm start
+```
+
+## API
+The API is very simple. The service listens to GET request of the following format:
+```
+/[zone]/[cmd]
+```
+where
+- **zone** is any of the values *1*, *2*, *3*, *4*, or *all*
+- **cmd** is any of the following:
+  - *on* to turn the zone on
+  - *off* to turn the zone off
+  - *white* followed by a dash ('-') and a value between 0 to 100 to set the zone to white with the given intensity
+    - example: `/1/white-50` sets the zone 1 to a 50% white light
+  - *bright* followed by a dash ('-') and a value between 0 to 100 to set the brightness of the zone
+    - example: `/all/bright-30` sets the brightness of all zones to 30%
+  - *rgb* followed by a dash ('-') and a value of the form *rrggbb* to set the zone's RGB values
+    - example: `/2/rgb/ffff00` sets the color of zone 2 to orange
+
+## Configuration
+The configuration file `config.json` is optional and currently the following options are supported:
+- `milight_host` *String* The hostname of the milight module. Default: 'milight'
+- `http_port` *Number* The HTTP port to which the server will bind.
+
+## Acknowledgement
+Thanks to [Olivier Oeuillot](https://github.com/oeuillot) for the [node-milight](https://github.com/oeuillot/node-milight) module.
+
+## LICENSE
+[BSD 2-Clause](https://opensource.org/licenses/BSD-2-Clause)
---
+++ milight_server.js
@@ -0,0 +1,152 @@
+/*
+ A simple service offering an HTTP front to milight controllers.
+*/
+'use strict';
+
+var express = require('express');
+var app = express();
+var jsonfile = require('jsonfile');
+var extend = require('extend');
+var Milight = require("milight");
+var milight;
+
+const configFileName = '/config.json';
+var config = {
+  milight_host: "milight",
+  http_port: 8030
+};
+
+// --- setup the events system
+const EventEmitter = require('events');
+const util = require('util');
+
+function EventController() {
+  EventEmitter.call(this);
+}
+util.inherits(EventController, EventEmitter);
+
+const events = new EventController();
+
+// --- load config file
+events.on('configLoaded', setupServer);
+
+var file = __dirname + configFileName;
+jsonfile.readFile(file, function(err, obj) {
+  if (err) {
+    console.error('Could not read config file. Using default values.');
+    console.dir(err);
+  } else {
+    config = extend(true, {}, config, obj);
+    console.log('Configuration file loaded.');
+    console.dir(config);
+    events.emit('configLoaded');
+  }
+});
+
+function setupServer() {
+  milight = new Milight({
+      host: config.milight_host,
+      broadcast: true
+  });
+
+  app.get('/:zone/:cmd', milightService);
+
+  app.listen(config.http_port, function () {
+    console.log('Milight controller app listening on port %s!', config.http_port);
+  });
+}
+
+function milightService(req, res) {
+  var msg = {msg: 'All good!', status: 'OK'};
+  var zone = req.params.zone;
+  var cmd_full = req.params.cmd
+  var param;
+  param = undefined;
+
+  var pieces = cmd_full.split('-');
+  var cmd = pieces[0];
+  if (pieces.length > 1) {
+    param = pieces[1];
+  }
+  msg = updateZone(zone, cmd, param);
+
+  if (msg.status == "ERROR") {
+    res.status(400).send(msg);
+  } else {
+    res.send(msg);
+  }
+}
+
+function updateZone(zone, cmd, param) {
+  var mzone;
+
+  // set zone
+  if (zone == 'all') {
+    mzone = milight.allZones();
+  } else {
+    mzone = milight.zone(parseInt(zone));
+  }
+
+  var merror;
+  merror = undefined;
+
+  function onError(error) {
+    merror = error;
+  }
+
+  function checkParam(str) {
+    return (str !== undefined) && (str.length > 0);
+  }
+
+  function intParam(str) {
+    if (checkParam(str)) {
+      var result = parseInt(str);
+      if (isNaN(result)) {
+        merror = "'" + str + "' is not a numeric value.";
+        return undefined;
+      } else {
+        return result;
+      }
+    }
+  }
+
+  switch (cmd) {
+    case 'on':
+      mzone.on(onError);
+      break;
+
+    case 'off':
+      mzone.off();
+      break;
+
+    case 'white':
+      var mparam = intParam(param);
+      if (mparam !== undefined) {
+        mzone.white(mparam);
+      }
+      break;
+
+    case 'bright':
+      mparam = intParam(param);
+      if (mparam !== undefined) {
+        mzone.brightness(mparam);
+      }
+      break;
+
+    case 'rgb':
+      if (checkParam(param)) {
+        mparam = '#' + param;
+        mzone.rgb(mparam);
+      }
+      break;
+
+    default:
+      return {status: "ERROR", msg: "Command '" + cmd + "' not found."};
+  }
+
+  if (merror) {
+    return {status: "ERROR", msg: merror};
+  } else {
+    return {status: "OK", msg: "Done."};
+  }
+}
